c1879aa64e4b54c17bdb9ad9906bebd6fb32db1c
Bit-ByteBandit <134239703+Bit-ByteBandit@users.noreply.github.com>
2024-02-08 22:35:55 +0300

    adding Command line arguments support instead of path

--- decrypt_chrome_password.py
+++ decrypt_chrome_password.py
@@ -9,15 +9,12 @@ import win32crypt
 from Cryptodome.Cipher import AES
 import shutil
 import csv
-
-#GLOBAL CONSTANT
-CHROME_PATH_LOCAL_STATE = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data\Local State"%(os.environ['USERPROFILE']))
-CHROME_PATH = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data"%(os.environ['USERPROFILE']))
+import sys
 
 def get_secret_key():
-    try:
+    
         #(1) Get secretkey from chrome local state
-        with open( CHROME_PATH_LOCAL_STATE, "r", encoding='utf-8') as f:
+        with open( sys.argv[2], "r", encoding='utf-8') as f:
             local_state = f.read()
             local_state = json.loads(local_state)
         secret_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
@@ -25,10 +22,7 @@ def get_secret_key():
         secret_key = secret_key[5:] 
         secret_key = win32crypt.CryptUnprotectData(secret_key, None, None, None, 0)[1]
         return secret_key
-    except Exception as e:
-        print("%s"%str(e))
-        print("[ERR] Chrome secretkey cannot be found")
-        return None
+   
     
 def decrypt_payload(cipher, payload):
     return cipher.decrypt(payload)
@@ -72,31 +66,30 @@ if __name__ == '__main__':
             #(1) Get secret key
             secret_key = get_secret_key()
             #Search user profile or default folder (this is where the encrypted login password is stored)
-            folders = [element for element in os.listdir(CHROME_PATH) if re.search("^Profile*|^Default$",element)!=None]
-            for folder in folders:
+            
             	#(2) Get ciphertext from sqlite database
-                chrome_path_login_db = os.path.normpath(r"%s\%s\Login Data"%(CHROME_PATH,folder))
-                conn = get_db_connection(chrome_path_login_db)
-                if(secret_key and conn):
-                    cursor = conn.cursor()
-                    cursor.execute("SELECT action_url, username_value, password_value FROM logins")
-                    for index,login in enumerate(cursor.fetchall()):
-                        url = login[0]
-                        username = login[1]
-                        ciphertext = login[2]
-                        if(url!="" and username!="" and ciphertext!=""):
-                            #(3) Filter the initialisation vector & encrypted password from ciphertext 
-                            #(4) Use AES algorithm to decrypt the password
-                            decrypted_password = decrypt_password(ciphertext, secret_key)
-                            print("Sequence: %d"%(index))
-                            print("URL: %s\nUser Name: %s\nPassword: %s\n"%(url,username,decrypted_password))
-                            print("*"*50)
-                            #(5) Save into CSV 
-                            csv_writer.writerow([index,url,username,decrypted_password])
+            
+            conn = get_db_connection(sys.argv[1])
+            if(secret_key and conn):
+                cursor = conn.cursor()
+                cursor.execute("SELECT action_url, username_value, password_value FROM logins")
+                for index,login in enumerate(cursor.fetchall()):
+                    url = login[0]
+                    username = login[1]
+                    ciphertext = login[2]
+                    if(url!="" and username!="" and ciphertext!=""):
+                        #(3) Filter the initialisation vector & encrypted password from ciphertext 
+                        #(4) Use AES algorithm to decrypt the password
+                        decrypted_password = decrypt_password(ciphertext, secret_key)
+                        print("Sequence: %d"%(index))
+                        print("URL: %s\nUser Name: %s\nPassword: %s\n"%(url,username,decrypted_password))
+                        print("*"*50)
+                        #(5) Save into CSV 
+                        csv_writer.writerow([index,url,username,decrypted_password])
                     #Close database connection
-                    cursor.close()
-                    conn.close()
+                cursor.close()
+                conn.close()
                     #Delete temp login db
-                    os.remove("Loginvault.db")
+                os.remove("Loginvault.db")
     except Exception as e:
-        print("[ERR] %s"%str(e))
+        print("\n" + sys.argv[0]+ " [login data] [login state]")
